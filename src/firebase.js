40393d836948f999f875505d5973d3bd9f82c646
Truonghq97 <truonghq97@gmail.com>
2020-10-13 15:56:25 +0700

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALgnI6rxSas-He_S7BL5Qf127po1wCuJU",
+  authDomain: "discord-clone-a4d1e.firebaseapp.com",
+  databaseURL: "https://discord-clone-a4d1e.firebaseio.com",
+  projectId: "discord-clone-a4d1e",
+  storageBucket: "discord-clone-a4d1e.appspot.com",
+  messagingSenderId: "216115153891",
+  appId: "1:216115153891:web:42eea72a2fc8042e142ed6",
+  measurementId: "G-BCTS5HG20W"
+};
